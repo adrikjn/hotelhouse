0e6d9955bf5450780c50e7c39346d484de6706ed
Adrien KOUYOUMJIAN <adrien.kouyoumjian@outlook.fr>
2023-06-28 09:52:27 +0200

    push

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,9 +1,14 @@
-// const btn = document.querySelector('.btn');
-// const header = document.querySelector('header');
+const logoImpression = document.getElementById('logo-impression');
+  logoImpression.addEventListener('click', () => {
+    window.print();
+  });
 
 
-// const nav = document.querySelector("nav");
-// const toggleBtn = document.querySelector('.menu-hamburger');
-// toggleBtn.addEventListener('click', ()=> {
-//   nav.classList.toggle('show');
-// })
+  const lienPartage = document.getElementById('lien-partage');
+  lienPartage.addEventListener('click', (event) => {
+    event.preventDefault();
+    const url = encodeURIComponent(window.location.href);
+    const titre = encodeURIComponent(document.title);
+    const partageURL = `https://example.com/partager?url=${url}&titre=${titre}`;
+    window.open(partageURL, '_blank');
+  });
