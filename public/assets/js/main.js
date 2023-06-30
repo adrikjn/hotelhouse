a950d54aab9319b4349f5e447efe5e13eafdf3d9
Adrien KOUYOUMJIAN <adrien.kouyoumjian@outlook.fr>
2023-06-30 14:27:00 +0200

    push

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,36 +1,37 @@
-const logoImpression = document.getElementById('logo-impression');
-  logoImpression.addEventListener('click', () => {
-    window.print();
-  });
+const logoImpression = document.getElementById("logo-impression");
+logoImpression.addEventListener("click", () => {
+  window.print();
+});
 
+const lienPartage = document.getElementById("lien-partage");
+const urlCopie = document.getElementById("url-copie");
+lienPartage.addEventListener("click", (event) => {
+  event.preventDefault();
+  const url = window.location.href;
+  urlCopie.value = url;
+  urlCopie.select();
+  document.execCommand("copy");
+  alert(
+    "Lien copié avec succès ! Vous pouvez le coller et le partager avec vos amis."
+  );
+});
 
-  const lienPartage = document.getElementById('lien-partage');
-  lienPartage.addEventListener('click', (event) => {
+const spaLinks = document.querySelectorAll(".spa-link");
+const spaSections = document.querySelectorAll(".spa-section");
+
+// Ajouter la classe "show" à la première section par défaut
+spaSections[0].classList.add("show");
+
+spaLinks.forEach((link, index) => {
+  link.addEventListener("click", (event) => {
     event.preventDefault();
-    const url = encodeURIComponent(window.location.href);
-    const titre = encodeURIComponent(document.title);
-    const partageURL = `https://househotel.osc-fr1.scalingo.io/?url=${url}&titre=${titre}`;
-    window.open(partageURL, '_blank');
-  });
-  
+    const target = link.dataset.target;
 
-  const spaLinks = document.querySelectorAll('.spa-link');
-  const spaSections = document.querySelectorAll('.spa-section');
-  
-  // Ajouter la classe "show" à la première section par défaut
-  spaSections[0].classList.add('show');
-  
-  spaLinks.forEach((link, index) => {
-    link.addEventListener('click', (event) => {
-      event.preventDefault();
-      const target = link.dataset.target;
-  
-      spaSections.forEach(section => {
-        section.classList.remove('show');
-      });
-  
-      const targetSection = document.getElementById(target);
-      targetSection.classList.add('show');
+    spaSections.forEach((section) => {
+      section.classList.remove("show");
     });
+
+    const targetSection = document.getElementById(target);
+    targetSection.classList.add("show");
   });
-  
+});
